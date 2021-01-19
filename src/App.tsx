aac2a9b0ad14a4dbb34043e174a374b206630f8c
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-19 17:04:23 +0500

    Create Footer Component
    1:- Created
    	a:- src\components\Footer.tsx to define header component
    2:- Updated
    	a:- src\App.tsx to add footer component in the app
    	b:- src\app.css to define styling of footer app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 // Component Imports
 import Header from './components/Header';
+import Footer from './components/Footer';
 // Styles Imports
 import './App.css';
 
@@ -10,8 +11,8 @@ function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
-    
   );
 }
 
